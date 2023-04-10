5469abb8291e68fa34acee85608555c31a67c831
Josh Hilliard <125423113+jghilliard@users.noreply.github.com>
2023-04-10 17:22:01 -0500

    working on printer

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -77,7 +77,7 @@ public class TechJobs {
         Boolean validChoice = false;
         String[] choiceKeys = new String[choices.size()];
 
-        // Put the choices in an ordered structure so we can
+        // Put the choices in an ordered structure so that we can
         // associate an integer with each one
         int i = 0;
         for (String choiceKey : choices.keySet()) {
@@ -119,7 +119,18 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        if (someJobs.size() ==0){
+            System.out.println("No Results");
+        } else {
+            for (HashMap<String, String> entry : someJobs) {
+                System.out.println("*****");
+                for (String key : entry.keySet()) {
+                    System.out.println(key + ": " + entry.get(key));
+                }
+                System.out.println("*****\n");
+            }
 
-        System.out.println("printJobs is not implemented yet");
+        }
+
     }
 }
