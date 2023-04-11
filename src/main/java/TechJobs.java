6e2395734072a3112197525a83df01e07bcc28f7
Josh Hilliard <125423113+jghilliard@users.noreply.github.com>
2023-04-11 17:38:12 -0500

    printer and findbyvalue done.

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,15 +119,15 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-        if (someJobs.size() ==0){
-            System.out.println("No Results");
+        if (someJobs.size() == 0){
+            System.out.print("No Results");
         } else {
             for (HashMap<String, String> entry : someJobs) {
-                System.out.println("*****");
+                System.out.println("\n*****");
                 for (String key : entry.keySet()) {
                     System.out.println(key + ": " + entry.get(key));
                 }
-                System.out.println("*****\n");
+                System.out.println("*****");
             }
 
         }
